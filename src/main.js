910e1bb9c30c09a40bd7111324d8c8dcec7124bc
Jun <jun126m@prestigein.com>
2023-12-01 17:10:13 +0900

    add:amplifyコンポーネントの日本語化とサインアップ項目削除とボタンの色変更

--- src/main.js
+++ src/main.js
@@ -9,15 +9,13 @@ import axios from 'axios'
 import VueAxios from 'vue-axios'
 import App from './App.vue'
 import router from './router'
-// import VueApexCharts from "vue3-apexcharts"
-
 
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
 app.use(vuetify)
-// app.use(VueApexCharts)
 app.use(VueAxios, axios)
+
 
 app.mount('#app')
--- src/views/ChartView.vue
+++ src/views/ChartView.vue
@@ -1,33 +1,66 @@
 <template>
   <v-container>
     <h2>チャート</h2>
-<apexchart
-  type="pie"
-  height="400"
-  :options="chart.options"
-  :series="chart.series"
-></apexchart>
+    <apexchart
+        type="pie"
+        height="400"
+        :options="chart.options"
+        :series="chart.series"
+    ></apexchart>
+
+    <apexchart
+        type="bar"
+        height="400"
+        :options="chart_2.options"
+        :series="chart_2.series"
+    ></apexchart>
+     <QuillEditor theme="snow" />
 
   </v-container>
 </template>
 <script>
 
 import VueApexCharts from 'vue3-apexcharts'
+import { QuillEditor } from '@vueup/vue-quill'
+import '@vueup/vue-quill/dist/vue-quill.snow.css';
 export default {
   components: {
     apexchart: VueApexCharts,
+    QuillEditor
   },
 
   data: () => ({
     chart: {
       options: {
-        labels: ['20代', '30代', '40代', '50代'],
+        labels: ['回答', '未回答'],
         title: {
-          text: 'あなたの年齢を教えてください。',
+          text: '回答率',
           align: 'center',
         },
       },
-      series: [20, 20, 50, 10],
+      series: [50, 10],
+    },
+    chart_2: {
+      options: {
+        plotOptions: {
+          bar: {
+            horizontal: true, // 横型のグラフにする場合
+          },
+        },
+        title: {
+          text: '安否状況',
+          align: 'center',
+        },
+        xaxis: {
+          categories: ['安全', '軽傷', '重症', '未回答'],
+        },
+      },
+      series: [
+        {
+          name: '件数',
+          data: [30, 5, 1, 4],
+        },
+      ],
     },
   }),
 }
